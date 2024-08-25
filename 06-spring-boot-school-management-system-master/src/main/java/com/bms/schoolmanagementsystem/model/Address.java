0a6f41518b666f163c59539ee98c7d733666908e
Adinath75 <puriadinath75@gmail.com>
2024-08-25 22:43:24 +0530

    asdfg

---
+++ 06-spring-boot-school-management-system-master/src/main/java/com/bms/schoolmanagementsystem/model/Address.java
@@ -0,0 +1,30 @@
+package com.bms.schoolmanagementsystem.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id", referencedColumnName = "id")
+    private Student student;
+
+}
